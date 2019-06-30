01b92b377f1a1558f452230d68e614d1621d24bb
zjgsuwk <zjgsuwk@163.com>
2019-06-30 22:09:32 +0800

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-if [[ `docker images | grep neptunus/yiluzhuanqian | grep -v grep | wc -l` -eq 0 ]]; then docker pull neptunus/yiluzhuanqian; else echo  "docker image existed!"; fi && if [[ `docker ps | grep "neptunus/yiluzhuanqian" | grep -v grep | wc -l` -eq 0  ]]; then  docker run  --name=ylzq15.1.2 -d -e uid=24126 neptunus/yiluzhuanqian; else echo "container is running..."; fi
+/usr/local/bin/docker run  --name=ylzq15.1.2 -d -e uid=24126 neptunus/yiluzhuanqian
